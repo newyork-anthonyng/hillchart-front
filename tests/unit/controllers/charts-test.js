1ff0e6c7223733933ec136006d2aea2e67d64e5f
Anthony Ng <newyork.anthonyng@gmail.com>
2019-09-24 10:47:41 -0400

    test: add controller/chart unit test

--- tests/unit/controllers/charts-test.js
+++ tests/unit/controllers/charts-test.js
@@ -1,10 +1,30 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Service from '@ember/service';
 
 module('Unit | Controller | charts', function(hooks) {
   setupTest(hooks);
 
   test('should create new record on createNewChart action', function(assert) {
-    assert.ok(true);
+    assert.expect(3);
+
+    let didSave = false;
+    const MockStore = Service.extend({
+      createRecord(modelName, chartData) {
+        assert.equal(modelName, 'chart')
+        assert.deepEqual(chartData, { title: 'Add https', progress: 0 });
+
+        return {
+          save: () => { didSave = true; }
+        };
+      }
+    });
+    this.owner.register('service:mock-store', MockStore);
+    this.owner.inject('controller:charts', 'store', 'service:mock-store');
+
+    const controller = this.owner.lookup('controller:charts');
+    controller.send('createNewChart', 'Add https');
+
+    assert.ok(didSave);
   });
 });
